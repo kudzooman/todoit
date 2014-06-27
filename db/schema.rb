2c4f27d28a40b14e49edb4546aa8c7aec758542c
Chris Elwood <kudzooman@gmail.com>
2014-06-26 20:06:25 -0700

    redo on db with lists

--- db/schema.rb
+++ db/schema.rb
@@ -11,16 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140624193115) do
+ActiveRecord::Schema.define(version: 20140627030238) do
 
   create_table "lists", force: true do |t|
     t.string   "name"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.integer  "user_id"
+    t.integer  "users_id"
   end
-
-  add_index "lists", ["user_id"], name: "index_lists_on_user_id"
 
   create_table "todos", force: true do |t|
     t.string   "item"
